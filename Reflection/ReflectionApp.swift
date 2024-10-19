f238f72da37b91978b43ce1f725f76d11b058bee
aravindhramesh <aravindh97@gmail.com>
2024-10-19 16:13:29 -0700

    Initial Commit

---
+++ Reflection/ReflectionApp.swift
@@ -0,0 +1,20 @@
+//
+//  ReflectionApp.swift
+//  Reflection
+//
+//  Created by Aravindh Ramesh on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReflectionApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
